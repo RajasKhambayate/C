220de159d085cca444873c233133f7671a177b9f
RajasKhambayate <rajaskhambayate8@gmail.com>
2025-05-15 22:49:43 +0530

    This program displays factors of a number .

--- Problems_on_Numbers/Factors.c
+++ Problems_on_Numbers/Factors.c
@@ -1,7 +1,33 @@
-//This code displays factors of a positive number.
-#include<stdio.h>
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//Description: This program displays factors of a number .                                        //
+//================================================================================================//
+//Language: C                                                                                     //
+//Compiler : GNU GCC                                                                              //
+//IDE: Visual Studio code                                                                         //
+//================================================================================================//
+//Author/Coder: Rajas Khambayate                                                                  //
+//Date: 16th May 2025                                                                             //
+//Day: Friday                                                                                     //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>//for printf and scanf functions .
 
-void Factors(int iValue)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//Factors() Function: This function displays factors of a number.                                 //
+//================================================================================================//
+//Parameters:                                                                                     //
+//1. int(iValue) : The value for calculating factors .                                            //
+//================================================================================================//
+//Return: void                                                                                    //
+//================================================================================================//
+//Local variables: None                                                                           //
+//================================================================================================//
+//Algorithm:                                                                                      //
+//1. Loop 'iCnt' from 1 to 'iValue':                                                              //
+//   a. If 'iValue' is divisible by 'iCnt', print 'iCnt'.                                         //
+//2. End of loop.                                                                                 //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void Non_Factors(int iValue)
 {
     for(int iCnt = 1;iCnt <= iValue;iCnt++)
     {
@@ -12,22 +38,36 @@ void Factors(int iValue)
     }
 }
 
+//Starting point of application
 int main()
 {
     int iNo = 0;
 
-    while(iNo <= 0)
+    while(1)
     {
-        printf("\n\nPlease enter a positive integer value : ");
+        iNo = 0;
+
+        while(iNo <= 0)
+        {
+            printf("Please enter a positive integer value : ");
+            scanf("%d",&iNo);
+        }
+
+        printf("Would you like to try Non_Factors one more time? >Press for->YES:ANY_NUM OR NO:0 <=>Your Choice : ");
         scanf("%d",&iNo);
 
-        if(iNo <= 0)
+        if(iNo == 0)
         {
-            printf("Please enter a positive number\n");
+            printf("====================================================================================================\n");
+            printf("Thank you for using Rajas's Non_Factors program .\n");
+            break;
+        }
+        else
+        {
+            printf("====================================================================================================\n");
+            continue;
         }
     }
-
-    Factors(iNo);
 
     return 0;
 }
